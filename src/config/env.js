90aabdbfba2046e4a08eeb6a0da415d11fe58c4d
Briantoroflh <briantoroflh@gmail.com>
2026-07-18 02:29:57 +0700

    test console log

--- src/config/env.js
+++ src/config/env.js
@@ -1,11 +1,13 @@
 import "dotenv/config"
 
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
-
 var apiKey = process.env.OPENROUTER_API_KEY
 var model = process.env.OPENROUTER_MODEL
+
+console.log("Environment check:", {
+    hasApiKey: Boolean(apiKey),
+    hasModel: Boolean(model),
+    model
+});
 
 if(!apiKey) {
     throw new Error("OPENROUTER_API_KEY not found in .env")
